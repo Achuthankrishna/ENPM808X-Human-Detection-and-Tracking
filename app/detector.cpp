c40c90ddb6377729ba56090a24e8aa7dbaf273fa
Achuthankrishna <achkrish07@gmail.com>
2023-10-24 19:32:36 -0400

    Added detector class in CPP and some comments

---
+++ app/detector.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file detector.cpp
+ * @author Vyshnav Achuthan (vyachu07@umd.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include "HumanDetector.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <opencv2/dnn.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <sstream>
+Detector::Detector(){
+    return 0;
+}
--- include/Camera.hpp
+++ include/Camera.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file Camera.hpp
+ * @author Vyshnav Achuthan (vyachu07@umd.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #pragma once
 
 #include<iostream>
