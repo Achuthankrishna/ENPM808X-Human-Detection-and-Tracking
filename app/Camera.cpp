5ce38bb38d7c1f4c13b228a058e99c1b442ce2f5
Achuthankrishna <achkrish07@gmail.com>
2023-10-24 21:36:39 -0400

    1.5.1 Stubs for Camera Class 2

---
+++ app/Camera.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file Camera.hpp
+ * @author Vyshnav Achuthan (vyachu07@umd.edu)
+ * @brief
+ * @version 0.1
+ * @date 2023-10-24
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#pragma once
+
+#include "../include/camera.hpp"
+
+Camera::Camera(){
+    get_video = cv::VideoCapture(0);
+}
+
+Camera::~Camera(){
+    get_video.release();
+}
