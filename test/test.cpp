125bd43b934054b1c33547cd3a1c5ff5d6c76d29
Achuthankrishna <achkrish07@gmail.com>
2023-10-24 22:31:22 -0400

    1.6.2 Created Test.cpp for Dependency test

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,22 @@
 #include <gtest/gtest.h>
+#include <opencv2/opencv.hpp>
 
 
 TEST(dummy_test, this_should_pass) {
   EXPECT_EQ(1, 1);
+}
+/**
+ * @brief Construct a new TEST object for OpenCV
+ * 
+ */
+TEST(OpenCVTest, LoadImage) {
+    // Load an image from file
+    std::string imagePath = __FILE__;
+    imagePath = imagePath.substr(0, imagePath.find_last_of("/\\") + 1) + "../app/test.jpg";
+    cv::Mat image = cv::imread(imagePath);
+
+    // Check if the image was loaded successfully
+    EXPECT_FALSE(image.empty());
 }
 
 // TEST(dummy_test, this_should_pass_too) {
