1b96fce3db9514dd104b2859cf692f24e4c2c744
Achuthankrishna <achkrish07@gmail.com>
2023-10-24 22:06:22 -0400

    1.5.3 Added detector class stub ,fixed opencv inc

--- app/detector.cpp
+++ app/detector.cpp
@@ -1,12 +1,12 @@
 /**
  * @file detector.cpp
  * @author Vyshnav Achuthan (vyachu07@umd.edu)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-10-24
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #include "HumanDetector.hpp"
 
@@ -15,17 +15,11 @@
 #include <opencv2/dnn.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 #include <sstream>
+
 Detector::Detector(){
-    return 0;
 }
-void Detector::processing(Mat& frame, const vector<Mat> & output){
-
-}
-void Detector::drawboxes(int classID, float con, int left,
-        int right, int bottom, Mat& frame){
-
-        }
-vector<String> Detector::ClassNames(const Net& net){
+Detector::~Detector(){
 
 }
--- include/Camera.hpp
+++ include/Camera.hpp
@@ -18,7 +18,7 @@
 #include<algorithm>
 #include <opencv2/dnn.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2\opencv.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 
 using cv;
