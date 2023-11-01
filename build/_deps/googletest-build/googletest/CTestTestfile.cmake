28f9b8eaca4230d5cf183e36eb1f0ae4754be62f
interns <achkrish07@gmail.com>
2023-11-01 17:26:27 -0400

    2.9 Cpp Lint and Cppcheck

--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/interns/prim/ENPM808X-Human-Detection-and-Tracking/build/_deps/googletest-src/googletest
-# Build directory: /home/interns/prim/ENPM808X-Human-Detection-and-Tracking/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/interns/prim/ENPM808X-Human-Detection-and-Tracking/app
-# Build directory: /home/interns/prim/ENPM808X-Human-Detection-and-Tracking/build/app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
