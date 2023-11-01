7ed157d655d1fa327cd8f638f2ed4c58dec9c104
Achuthankrishna <achkrish07@gmail.com>
2023-11-01 00:25:06 -0400

    2.5.1 camera class implementation

--- app/camera.cpp
+++ app/camera.cpp
@@ -1,38 +1,56 @@
 /**
- * @file Camera.hpp
+ * @file camera.cpp
  * @author Vyshnav Achuthan (vyachu07@umd.edu)
  * @author Kiran S Patil (kpatil27@umd.edu)
  * @author Surya Chappidi (chappidi@umd.edu)
- * @brief
+ * @brief camera class implementation
  * @version 0.1
  * @date 2023-10-24
  *
  * @copyright Copyright (c) 2023
  *
  */
-#pragma once
 
 #include "../include/camera.hpp"
+
 /**
  * @brief Construct a new Camera:: Camera object
  *
  */
-Camera::Camera() {
-    get_video = cv::VideoCapture(0);
+Camera::Camera(std::string videoPath) {
+    get_video.open(videoPath);
+    if (!get_video.isOpened()) {
+        std::cout << "Error opening video stream or file" << std::endl;
+        throw("Camera Not Captured");
+    }
 }
+
+
 /**
  * @brief Destroy the Camera:: Camera object
  *
  */
 Camera::~Camera() {
-    get_video.release();
+    // get_video.release();
+    if (get_video.isOpened()) {
+        get_video.release();
+    }
 }
+
+
 /**
  * @brief Method to read the camera frames obtained
  *
  * @return cv::Mat
  */
 cv::Mat Camera::ReadFrame() {
-    get_video.read(get_frame);
+    cv::Mat get_frame;
+    get_video >> get_frame;
+    if (get_frame.empty()) throw("Not able to read frame");
     return get_frame;
 }
+
+
+
+
+
