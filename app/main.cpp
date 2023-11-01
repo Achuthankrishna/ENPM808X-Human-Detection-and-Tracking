849b5ea00d25db8449019e3db97b5b74f2a07511
kirangit27 <kiran.suvas.patil@gmail.com>
2023-11-01 02:27:26 -0400

    2.6 update main

--- app/main.cpp
+++ app/main.cpp
@@ -11,35 +11,15 @@
  *
  */
 
-#include "../include/camera.hpp"
-#include "../include/detector.hpp"
-#include "../include/tracker.hpp"
+
 #include "../include/robot.hpp"
 
 
 int main() {
 
-    float data[16] = {0, 0, -1, 0, 1, 0, 0, 0.5, 0, -1, 0, 0.5, 0, 0, 0, 1};
-    cv::Mat emptyFrame;
-    cv::VideoWriter videoWriter("human.avi", cv::VideoWriter::fourcc('X', 'V', 'I', 'D'), 30, cv::Size(640, 480), true);
-    std::vector<Detector::bbox> bboxs;
-
-    Robot robot;
-    Detector detector;
-    detector.load_model(detector.model_Cfg,detector.model_Wts,detector.c_path);
-    std::pair<cv::Mat, std::vector<Detector::bbox>> result=detector.detector(emptyFrame);
-    cv::Mat frame = result.first;
-
-    std::vector<Detector::bbox> bbox = result.second;
-    Tracker tracker;
-    tracker.getPredictions(frame,bbox);
-    tracker.boundingBox(frame,bbox);
-
-
-
-
+Robot robot;
+    robot.humanDetection();
 
     return 0;
-
 
 }
