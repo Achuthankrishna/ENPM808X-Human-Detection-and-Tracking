a67d7b347290ec06371ce19227226e42a6669543
kirangit27 <kiran.suvas.patil@gmail.com>
2023-11-01 02:32:50 -0400

    2.7.1 added unit test for camera

---
+++ test/test_cam.cpp
@@ -0,0 +1,26 @@
+#include "../include/camera.hpp"
+
+#include <gtest/gtest.h>
+
+
+TEST(CameraTest, ReadFrame) {
+    // Create a temporary video file for testing (you can replace this with an actual video file)
+    cv::VideoCapture test_video;
+    cv::Mat test_frame;
+    test_video.open(0);
+    test_video >> test_frame;
+    cv::imwrite("test_frame.jpg", test_frame);
+
+    // Instantiate the Camera object
+    Camera test_camera("test_frame.jpg");
+
+    // Read a frame from the camera
+    cv::Mat result_frame = test_camera.ReadFrame();
+
+    // Compare the dimensions of the frames
+    EXPECT_EQ(test_frame.rows, result_frame.rows);
+    EXPECT_EQ(test_frame.cols, result_frame.cols);
+
+    // Delete the temporary video file
+    remove("test_frame.jpg");
+}
