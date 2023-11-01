c0a865a088cffb99146d9940e074470f318e9077
SuryaVC <144150950+SuryaVC@users.noreply.github.com>
2023-11-01 09:01:21 -0400

    Update tracker.cpp

--- app/tracker.cpp
+++ app/tracker.cpp
@@ -15,7 +15,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/tracking.hpp>
-#include <opencv2/tracker.hpp>
 #include "../include/tracker.hpp"
 #include "../include/detector.hpp"
 /**
